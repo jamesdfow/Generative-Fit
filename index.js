358ee52e9a8da48678aa6a3a226e1ee56ead0c04
James <160443991+jamesdfow@users.noreply.github.com>
2026-03-31 22:51:14 -0600

    cleaned up index.js to be a bare Express API server. created db.js to handle the PostgreSQL connection pool. intalled express, pg, and dotenv. set up .env with the DB url. setup .gitignore to protect the .env

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require('dotenv').config()
+const db = require('./db')
 const express = require('express');
-
 const app = express();
 const port = 3010;
 
+
 app.use(express.json());
-
-
 app.listen(port, function (err) {
     if (err) console.log(err);
     console.log("Server listening on port", port);
